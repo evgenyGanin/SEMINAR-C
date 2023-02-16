4e667872c9d7a7d6854227b86b1177d499037ef5
evGenyGanin <ufolklore@yandex.ru>
2023-02-16 23:51:27 +0300

    init commit

---
+++ Seminar01/Homework_01/Program.cs
@@ -0,0 +1,68 @@
+﻿// Console.Clear();
+// Console.WriteLine("Введите первое число: ");
+// int numA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int numB = Convert.ToInt32(Console.ReadLine());
+
+// if (numA > numB) Console.WriteLine($"Число {numA} больше. Число {numB} меньше.");
+// else Console.WriteLine($"Число {numB} больше. Число {numA} меньше.");
+// ---------------------------------
+// Console.Clear();
+// Console.WriteLine("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// int max = a;
+
+// if (b > a) max = b; 
+// if (c > b) max = c;
+// Console.WriteLine($"Максимальное из введенных вами чисел: {max}");
+// ---------------------------
+// Console.Clear();
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num%2 == 0) 
+// {
+//     Console.WriteLine("Число является четным");
+// }
+// else 
+// {
+//     Console.WriteLine("Число не является четным");
+// }
+// ---------------------
+// Console.Clear();
+// Console.WriteLine("Введите натуральное число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+
+// int x =1;
+// if (a > 0)
+// {
+//     Console.WriteLine($"Четные числа от 1 до {a}: ");
+//     while (x < a)
+
+//     {
+//         Console.Write(x+1+" ");
+//         x+=2;
+//     }
+// }
+// else
+// {
+//     System.Console.WriteLine("Вы ввели не натуральное число. Введите НАТУРАЛЬНОЕ число!");
+// }
+
+// int index = 2;
+
+// if (numN >= index);
+// while (index < numN)
+// {
+//     Console.Write(index);
+//     index=index+2;
+// }
+// // else
+// // {
+// //     System.Console.WriteLine("Число не соответствует условию задачи. Введите НАТУРАЛЬНОЕ число.");
+// }    
