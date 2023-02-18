d7934846e8fcbe26b1f266db2f6a7f9da98e0fb3
evGenyGanin <ufolklore@yandex.ru>
2023-02-18 20:11:20 +0300

    Added Homework #2 task10

---
+++ Seminar01/Task10 hw02 method/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 10: 
+// Напишите программу, которая:
+// 1. Принимает на вход трёхзначное число;
+// 2. На выходе показывает вторую цифру этого числа.
+
+// // 456 --> 5
+// // 782 --> 8
+// // 918 --> 1
+Console.Clear();
+void TaskText()
+{
+    System.Console.WriteLine("Задача 10:\nНапишите программу, которая: \n1. Принимает на входе трёхзначное число; \n2. На выходе показывает вторую цифру этого числа.\nПример:\n456 --> 5 \n782 --> 8 \n918 --> 1");
+}
+TaskText();
+// -------Task 10 Homework 2 Method-------------------------------------
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    int result = SecondNumber(number);
+    System.Console.WriteLine($"Второй цифрой введенного вами числа будет: {result}");
+}
+else
+{
+    System.Console.WriteLine("Вы ввели не трехзначное число. Повторите попытку...");
+}
+
+int SecondNumber(int num)
+{
+    int res = num / 10 % 10;
+    return res;
+}
