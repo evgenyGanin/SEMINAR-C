39dc5691a838a6bb279025b894435a543728568a
evGenyGanin <ufolklore@yandex.ru>
2023-02-25 22:58:59 +0300

    Add task19

---
+++ Seminar01/Task22 квадрат_числа/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Square(number);
+
+void Square (int num)
+{
+    int index = 1;
+    while (index <= num)
+    {
+    Console.WriteLine($"{index} в квадрате это ({index}x{index}) и равно: {index * index}");
+    index++;
+    }
+}
---
+++ Seminar01/Task23 hw03 куб_числа/Program.cs
@@ -0,0 +1,27 @@
+﻿// ЗАДАЧА 23: Напишите программу, которая:
+// 1. Принимает на вход число (N).
+// 2. Выдаёт таблицу кубов чисел от 1 до N.
+// 3 --> 1, 8, 27
+// 5 --> 1, 8, 27, 64, 125
+
+Console.Clear();
+void TaskText()
+{
+    System.Console.WriteLine("ЗАДАЧА 23: Напишите программу, которая:\n1. Принимает на вход число (N)\n2. Выдаёт таблицу кубов чисел от 1 до N.\n3 --> 1, 8, 27\n5 --> 1, 8, 27, 64, 125");
+}
+TaskText();
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Square(number);
+
+void Square (int num)
+{
+    int index = 1;
+    while (index <= num)
+    {
+    Console.WriteLine($"{index, 2} в кубе это ({index}x{index}x{index}) и равно: {index * index * index}");
+    index++;
+    }
+}
