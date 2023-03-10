2ca76b33490f607a082ffdadaa125e7dbd8f8afd
evGenyGanin <ufolklore@yandex.ru>
2023-03-10 16:09:13 +0300

    Homework 6

---
+++ Seminar01/Task41/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Random rand = new Random();
+int number = rand.Next(1, 11);
+PositiveNum((NewArray(number)), number);
+
+
+int[] NewArray(int number)
+{
+    int[] array = new int[number];
+    for (int i = 0; i < number; i++)
+    {
+        Console.Write($"Введите число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PositiveNum(int[] array, int number)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            count++;
+    }
+    System.Console.WriteLine($"{count} из {number} введённых чисел больше нуля.");
+}
+
+
---
+++ Seminar01/Task43/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 43: Напишите программу, 
+// которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+string k1 = "k1";
+double num1 = NumberEntering(k1);
+string b1 = "b1";
+double num2 = NumberEntering(b1);
+string k2 = "k2";
+double num3 = NumberEntering(k2);
+string b2 = "b2";
+double num4 = NumberEntering(b2);
+
+Intersection(num1, num2, num3, num4);
+
+double NumberEntering(string stroke)
+{
+    Console.Write($"Введите число {stroke}: ");
+    double number = Convert.ToDouble(Console.ReadLine());
+    return number;
+}
+
+void Intersection(double k1, double b1, double k2, double b2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Точка пересечения двух прямых: ({x}; {y})");
+}
+
+
