4ddc573ca422a4aac2b1143969b8b993146431f8
evGenyGanin <ufolklore@yandex.ru>
2023-03-17 20:12:56 +0300

    Homework 9

---
+++ Seminar01/Task64/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 64.
+//Задайте значения N. Напишите программу, которая выведет все
+//натуральные числа в промежутке от N до 1.
+//Выполнить с помощью рекурсии.
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int NatlNum(int n, int a)
+{
+    if(n == a) return n;
+    else
+    {
+        Console.Write($"{NatlNum(n, a + 1)}, ");
+        return a;
+    }
+}
+
+int EntDat(string text)
+{
+    Console.Write(text);
+    int n = int.Parse(Console.ReadLine());
+    return n;
+}
+
+int n = EntDat("Введите число: ");
+int a =1;
+Console.WriteLine(NatlNum (n, a));
---
+++ Seminar01/Task68/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 68.
+//Напишите программу вычисления функции Аккермана с помощью рекурсии.
+//Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+//m = 3, n = 2 -> A(m,n) = 29
+
+int m = EntDat("Введите значение m: ");
+int n = EntDat("Введите значение n: ");
+Console.WriteLine($"m={m}, n={n} -> A(m,n) = {AkkFunkcion(m, n)}");
+
+int EntDat(string text)
+{
+    Console.Write(text);
+    return int.Parse(Console.ReadLine());
+}
+
+int AkkFunkcion(int m, int n)
+{
+    if(m == 0) return n + 1;
+    if(m > 0 && n ==0) return AkkFunkcion (m - 1, 1);
+    else
+       return AkkFunkcion(m -1, AkkFunkcion(m, n - 1));  
+}
